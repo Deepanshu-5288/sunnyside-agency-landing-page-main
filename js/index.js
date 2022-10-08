171c6599361608ea54605afa610dc466fa3cf77b
Deeta-max <deepanshugupta5288@gmail.com>
2022-10-08 21:17:57 +0530

    final commit

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector("ul").classList.toggle("ul-hide");
+    document.querySelector(".triangle-left").classList.toggle("triangle-left-hide");
+})
